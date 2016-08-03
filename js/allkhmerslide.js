2a078dc2ce243c7f4da40534b7e866d762a56284
mineachem <minea.chem@gmail.com>
2016-08-03 14:31:02 +0700

    All File from All Khmer Docs

---
+++ js/allkhmerslide.js
@@ -0,0 +1,27 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "menu-basic") {
+        x.className += " responsive";
+    } else {
+        x.className = "menu-basic";
+    }
+}
+
+$(document).ready(function(){
+	
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
+
+})
+
+// $(document).ready(function(){
+// 	$
+// });
